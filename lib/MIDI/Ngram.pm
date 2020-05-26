c302009856d1c0ad9178fdc34253c666de484616
Gene Boggs <gene@ology.net>
2020-05-26 15:10:39 -0700

    Fix indent

--- lib/MIDI/Ngram.pm
+++ lib/MIDI/Ngram.pm
@@ -23,6 +23,7 @@ use Music::Note;
   my $mng = MIDI::Ngram->new(
     in_file      => [ 'eg/twinkle_twinkle.mid' ],
     ngram_size   => 3,
+    max_phrases  => 0, # Analyze all events
     patches      => [qw( 68 69 70 71 72 73 )],
     random_patch => 1,
   );
@@ -128,7 +129,8 @@ has durations => (
 
 =head2 patches
 
-The patches to choose from (at random) if given the B<random_patch> option.
+The patches to choose from (at random) if given the B<random_patch>
+option.
 
 Default: C<[0 .. 127]>
 
